4e6dd1606b1e5fcd8d22042061fe02eb51755fb8
Shirley Cudriz <shirley.cudriz@amerikatis.com>
2019-04-13 09:44:06 -0500

    commit 4

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+import { componentFactoryName } from "@angular/compiler";
+
+@Component({
+    selector : "app",
+    template:require("./app.component.html")
+})
+export class AppComponent{
+    nombre=""
+
+}
